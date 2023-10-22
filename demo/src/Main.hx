8d266da9df23f9df7470ae38f36e802c3977b01d
ThePlank <75984453+ThePlank@users.noreply.github.com>
2023-10-22 11:22:21 +0200

    add test project

---
+++ demo/src/Main.hx
@@ -0,0 +1,23 @@
+package;
+
+import sdl.GL;
+
+import sys.io.File;
+
+import lemons.App;
+import lemons.sound.Sound;
+
+class Main extends App {
+	var inst:Sound;
+	var voices:Sound;
+
+	override public function init():Void {
+		var sound:Sound = Sound.createFromBytes(File.getBytes('${Sys.getCwd()}\\res\\testsong.wav'));
+		sound.volume = 0.75;
+		sound.loop = true;
+		sound.play();
+	}
+
+	static public function main()
+		var app = new Main();
+}
